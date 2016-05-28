7d40396ea746d5110c05bcecde0f3b0b3577ab8f
Theo Harper <theo@bungyjar.com>
2016-05-28 23:19:11 +0100

    Initial Import

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,41 @@
-import { Component } from '@angular/core';
+import { Component }       from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My THIRD Angular 2 App</h1>'
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+   ROUTER_PROVIDERS,
+   HeroService
+  ]
 })
-export class AppComponent { }
 
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true    
+  },
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  }
+])
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
